cc2308a121850dcdd2d08a5a0dbbf71006e8f654
Andre Filipe <andre@easypay.pt>
2013-05-21 15:47:42 +0100

    #ADDED XML output

--- EasypayPaymentNotificationExample.php
+++ EasypayPaymentNotificationExample.php
@@ -9,7 +9,7 @@
  * @copyright	Easypay
  * @license	GPL v2
  */
-require_once('./api/Easypay.php');
+require_once( './api/Easypay.php' );
 
 /**
  * Define the easypay access parameters, 
@@ -17,17 +17,17 @@ require_once('./api/Easypay.php');
  * @var array
  */
 $easypay_params['user'] = $_GET['ep_user'];
-$easypay_params['cin'] = $_GET['ep_cin'];
+$easypay_params['cin'] 	= $_GET['ep_cin'];
 
 /**
  * Create a new Easypay Object
  * @var Easypay
  */
-$easypay = new Easypay($easypay_params);
+$easypay = new Easypay( $easypay_params );
 
 //set mode to either test or production.
 //This can be defined in the config file
-$easypay->set_live(false);
+$easypay -> set_live( true );
 
 //connecting to database
 /**
@@ -35,9 +35,9 @@ $easypay->set_live(false);
  * you may use your own connection system, this one is just for example purposes
  * you may also want to change your database credentials
  */
-$mysqli = new mysqli("localhost", "root", "123", "easypay_test");
-if ($mysqli->connect_errno) {
-    printf("Connection failed: %s\n", $mysqli->connect_error);
+$mysqli = new mysqli( "localhost", "root", "123", "easypay_test" );
+if ( $mysqli -> connect_errno ) {
+    printf( "Connection failed: %s\n", $mysqli -> connect_error );
     exit();
 }
 
@@ -50,28 +50,52 @@ if ($mysqli->connect_errno) {
 $query = "INSERT INTO `easypay_notifications` ( `ep_cin`, `ep_user`, `ep_doc`)
           VALUES ('" . $_GET['ep_cin'] . "', '" . $_GET['ep_user'] . "', '" . $_GET['ep_doc'] . "');";
 
-$mysqli->query($query);
+$mysqli -> query( $query );
 
 /**
  * Second, you will request the payment info and update the details of the requested payment
  */
-$info = $easypay->getPaymentInfo($_GET['ep_doc']);
+$info = $easypay -> getPaymentInfo( $_GET['ep_doc'] );
 
-$query = "UPDATE `easypay_notifications` SET
-            `ep_status` = '"        . $info['ep_status'] . "',
-            `ep_entity` = '"        . $info['ep_entity'] . "',
-            `ep_reference` = '"     . $info['ep_reference'] . "',
-            `ep_value` = '"         . $info['ep_value'] . "',
-            `ep_date` = '"          . $info['ep_date'] . "',
-            `ep_payment_type` = '"  . $info['ep_payment_type'] . "',
-            `ep_value_fixed` = '"   . $info['ep_value_fixed'] . "',
-            `ep_value_var` = '"     . $info['ep_value_var'] . "',
-            `ep_value_tax` = '"     . $info['ep_value_tax'] . "',
-            `ep_value_transf` = '"  . $info['ep_value_transf'] . "',
-            `ep_date_transf` = '"   . $info['ep_date_transf'] . "',
-            `t_key` = '"            . $info['t_key'] . "'
-          WHERE `ep_doc` = '" . $info['ep_doc'] . "';";
+if ( !$info ){
+	$status 	= 'err';
+	$message 	= 'Payment info fetch failed';
+} else {
+	
+	$query = "UPDATE `easypay_notifications` SET
+	            `ep_status` = '"        . $info['ep_status'] . "',
+	            `ep_entity` = '"        . $info['ep_entity'] . "',
+	            `ep_reference` = '"     . $info['ep_reference'] . "',
+	            `ep_value` = '"         . $info['ep_value'] . "',
+	            `ep_date` = '"          . $info['ep_date'] . "',
+	            `ep_payment_type` = '"  . $info['ep_payment_type'] . "',
+	            `ep_value_fixed` = '"   . $info['ep_value_fixed'] . "',
+	            `ep_value_var` = '"     . $info['ep_value_var'] . "',
+	            `ep_value_tax` = '"     . $info['ep_value_tax'] . "',
+	            `ep_value_transf` = '"  . $info['ep_value_transf'] . "',
+	            `ep_date_transf` = '"   . $info['ep_date_transf'] . "',
+	            `t_key` = '"            . $info['t_key'] . "'
+	          WHERE `ep_doc` = '" . $info['ep_doc'] . "';";
+	
+	if ( !$mysqli -> query( $query ) ){
+		$status 	= 'err';
+		$message 	= 'Update failed';
+	} else {
+		$status 	= 'ok';
+		$message 	= 'Generated doc';
+	}
+}
 
-$mysqli->query($query);
+$mysqli -> close();
 
-$mysqli->close();
+header( 'Content-type: text/xml' );
+
+?>
+<?= '<?xml version="1.0" encoding="ISO-8859-1" ?>' ?>
+<getautoMB_key>
+  <ep_status><?= $status ?></ep_status>
+  <ep_message><?= $message ?></ep_message>
+  <ep_cin><?= $_GET['ep_cin'] ?></ep_cin>
+  <ep_user><?= $_GET['ep_user'] ?></ep_user>
+  <ep_doc><?= $_GET['ep_doc'] ?></ep_doc>
+</getautoMB_key>
