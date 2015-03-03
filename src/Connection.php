7beee649798e23e699295c05d9acf36769f5a026
André Filipe <andre.r.flip@gmail.com>
2015-03-03 15:32:14 +0000

    Added phpdocs and handled the response data

--- src/Connection.php
+++ src/Connection.php
@@ -4,14 +4,28 @@ namespace Easypay\SDK;
 
 class Connection
 {
+    /**
+     * Configuration holder
+     * @var Configuration
+     */
     protected $config; 
 
+    /**
+     * Connection class constructor
+     * @param Configuration $config Configuration class
+     */
     public function __construct(Configuration $config)
     {
         $this->config = $config;
     }
 
-    public function call($segment, array $parameters = array())
+    /**
+     * Requests a specified segment of the API
+     * @param  string $segment    Segment
+     * @param  array  $parameters Arguments to be sent on the request
+     * @return [type]             [description]
+     */
+    public function call($segment, array $parameters = array(), $dataType = 'raw')
     {
         $url = $this->config->getEndpoint() . '_s/api_easypay_' . $segment . '.php';
         $urlParameters = array_merge($this->config->toArray(), $parameters);
@@ -22,7 +36,19 @@ class Connection
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $content = trim(curl_exec($ch));
+        $response = trim(curl_exec($ch));
         curl_close($ch);
+
+        switch ($dataType) {
+            case 'xml':
+                return json_decode(json_encode((array) simplexml_load_string($response), true));
+
+            case 'json':
+                return json_decode($response);
+            
+            case 'raw':
+            default:
+                return $response;
+        }
     }
 }
